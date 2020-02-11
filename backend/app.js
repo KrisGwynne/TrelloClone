7345464b5f17d83bc1781b80488d03767a9a6866
Kris Gwynne <krisgwynne@ymail.com>
2020-02-11 18:02:55 +0000

    Added routes to add a new ticket to the database, and find all tickets in the database

--- backend/app.js
+++ backend/app.js
@@ -1,20 +1,72 @@
 const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
+const bodyParser = require('body-parser');
+const cors = require('cors');
 require('dotenv').config();
-
 
 
 
 const app = express();
 const port = 9000;
 
+// app.use(cors())
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
 MongoClient.connect(process.env.url, function(err, client) {
     if (err) {
+        console.log(err);
         console.log('Error connecting to database');
     } else {
         console.log("Connected successfully to server");
        
-        const db = client.db('TrelloProjects');
+        const db = client.db('TrelloProjects').collection('Ticket');
+
+        app.route('/NewTicket')
+            .post((req,res,next) => {
+                const body = req.body;
+                db.insertOne({
+                    title: body.title,
+                    description: body.description,
+                    dueDate: body.dueDate,
+                }, (err, doc) => {
+                    if (err) {
+                        console.log('error added new ticket');
+                        console.log(err);
+                        res.send('Error adding new ticket');
+                    } else {
+                        console.log('added new ticket');
+                        next();
+                    }
+                })
+            }, (req,res) => {res.send('Successfully added new ticket')}
+        )
+
+        app.route('/FindTickets')
+            .get((req,res) => {
+                db.find({}).toArray((err, tickets) => {
+                    if (err) {
+                        console.log('error finding tickets');
+                        console.log(err);
+                    } else if (tickets){
+                        console.log('found tickets')
+                        console.log(tickets);
+                        res.send(tickets)
+                    } else {
+                        console.log('no tickets found')
+                    }
+                })
+            })
+
+        // db.findOne({name: 'test'}, (err,doc) => {
+        //     if (err) {
+        //         console.log('error');
+        //         console.log(err)
+        //     } else {
+        //         console.log(doc);
+        //     }
+        // })
     
         app.get('/', (req, res) => res.send('Hello World!'))
     }
