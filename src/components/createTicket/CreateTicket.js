9fdc05ecf5623a97180e045bd64e37e1dc2031e2
Kris Gwynne <krisgwynne@ymail.com>
2020-02-12 12:38:48 +0000

    added simple form to create tickets

--- src/components/createTicket/CreateTicket.js
+++ src/components/createTicket/CreateTicket.js
@@ -5,15 +5,62 @@ export default class CreateTicket extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            blank: null
+            title: '',
+            description: '',
         }
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(e) {
+        const target = e.target;
+        const name = target.name;
+
+        this.setState({
+            [name]: target.value,
+        })
+    }
+
+    handleSubmit(e) {
+        e.preventDefault()
+
+        if (this.state.title  === '') {
+            console.log('title cannot be blank')
+        }else if (this.state.description === '') {
+            console.log('description cannot be blank')
+        } else {
+            console.log('submitted')
+        }
+
     }
 
     render() {
         return (
             <div className='createTicket'>
-                <form>
-                    <imput type='text'></imput>
+                <form onSubmit={this.handleSubmit}>
+                    <label>
+                        Title: 
+                        <input
+                            name='title'
+                            type='text'
+                            value={this.state.value}
+                            onChange={this.handleChange} />
+                    </label>
+                    <br />
+                    <label>
+                        Description: 
+                        <input
+                            name='description'
+                            type='text'
+                            value={this.state.value}
+                            onChange={this.handleChange} />
+                    </label>
+                    <br />
+                    <input 
+                        type='submit'
+                        value='Submit'
+                    />
                 </form>
             </div>
         );
